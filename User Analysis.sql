e89d697b51db6fa76db636fdf05ce0b2975cba0d
Harsha330 <93589824+Harsha330@users.noreply.github.com>
2023-03-23 11:03:32 +0530

    Update User Analysis.sql

--- User Analysis.sql
+++ User Analysis.sql
@@ -13,6 +13,24 @@ SELECT *
 FROM website_pageviews;
 
 
+-- useage of date function 
+ SELECT 
+       order_items.order_id,
+       order_items.order_item_id ,
+       order_items.price_usd as price_paid_usd ,
+       order_items.created_at,
+
+order_item_refunds.order_item_refund_id,
+order_item_refunds.refund_amount_usd,
+order_item_refunds.created_at,
+
+datediff(order_item_refunds.created_at ,order_items.created_at) as days_to_refund
+FROM order_items LEFT JOIN order_item_refunds
+ON order_item_refunds.order_item_id = order_items.order_item_id
+WHERE order_items.order_id IN (3489,32049,27861);
+
+
+
 
 -- #1
 CREATE TEMPORARY TABLE repeat_sessions
